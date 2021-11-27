4e7d5dac6ede10fe281772a372747e1fe21eb51e
Yehor Fomin <yehor.fomin@nure.ua>
2021-11-27 20:42:56 +0200

    fisrt commit

---
+++ imageAugmentationParallel.py
@@ -0,0 +1,30 @@
+from PIL import Image, ImageFilter
+import time
+import multiprocessing
+file_names = ['4329937.jpeg', '9604597.jpeg', '9676155.jpeg',
+    '9676203.jpeg', '9979607.jpeg', '9980612.jpeg',
+    '10155843.jpeg', '10184043.jpeg', '10266847.jpeg',
+    '10275590.jpeg', '6665235.jpeg', '8902428.jpeg',
+    '9962634.jpeg', '10099964.jpeg', '10278125.jpeg',
+    '10288317.jpeg']
+start = time.perf_counter()
+size = (1200, 1200)
+def augment_image(img_name):
+    img = Image.open(img_name)
+    img = img.filter(ImageFilter.GaussianBlur(15))
+    img.thumbnail(size)
+    img.save(f'augmented-{img_name}')
+    print(f'{img_name} was augmented...')
+
+if __name__ == '__main__':
+    starttime = time.time()
+    processes = []
+    for f in file_names:
+        p = multiprocessing.Process(target=augment_image, args=(f,))
+        processes.append(p)
+        p.start()
+
+    for process in processes:
+        process.join()
+
+    print('That took {} seconds'.format(time.time() - starttime))
